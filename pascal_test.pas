4a41b2857050fcb275be35c56f6dc0de1d38aed7
Gustavo Isoo Binder <gustan.binder@gmail.com>
2023-05-23 16:23:35 -0300

    Feat: Added composed pieces.

--- pascal_test.pas
+++ pascal_test.pas
@@ -11,46 +11,202 @@ type tetramino = record
 		end;
 
 var ch: char;
-x, y, timer: integer;
+timer: integer;
 canPress : boolean;
 positions : matrix;
+current_block : tetramino;
 
-procedure insert_block(var positions : matrix; x, y : integer);
+{	TETRAMINO CREATION	}
+
+procedure create_i_block(var player : tetramino);
+var i : integer;
 begin
-	positions[x, y] := 1;
-	fpSystem('spd-say boop');
+	player.block_positions[1, 1] := 5;
+	player.block_positions[2, 1] := 4;
+	player.block_positions[3, 1] := 6;
+	player.block_positions[4, 1] := 7;
+	for i:=1 to 4 do
+		player.block_positions[i, 2] := 1;
 end;
 
-procedure check_collision(var positions: matrix; var x, y: integer);
+procedure create_t_block(var player : tetramino);
 begin
-	if positions[x, y + 2] = 1 then
-	begin
-		insert_block(positions, x, y + 1);
-		y := 1;
-		x := 5;
-	end;	
+	player.block_positions[1, 1] := 5;
+	player.block_positions[1, 2] := 1;
+	player.block_positions[2, 1] := 4;
+	player.block_positions[2, 2] := 1;
+	player.block_positions[3, 1] := 6;
+	player.block_positions[3, 2] := 1;
+	player.block_positions[4, 1] := 5;
+	player.block_positions[4, 2] := 2;
 end;
 
-procedure player_fall(var x, y: integer; var positions : matrix);
+procedure create_l_block(var player : tetramino);
 begin
-	if y + 2 >= HEIGHT then
-	begin
-		insert_block(positions, x, y + 2);
-		y := 1;
-		x := 5;
-	end
-	else
-	begin
-		y := y + 1;
+	player.block_positions[1, 1] := 5;
+	player.block_positions[1, 2] := 1;
+	player.block_positions[2, 1] := 4;
+	player.block_positions[2, 2] := 1;
+	player.block_positions[3, 1] := 6;
+	player.block_positions[3, 2] := 1;
+	player.block_positions[4, 1] := 6;
+	player.block_positions[4, 2] := 2;
+end;
+
+procedure create_j_block(var player : tetramino);
+begin
+	player.block_positions[1, 1] := 5;
+	player.block_positions[1, 2] := 1;
+	player.block_positions[2, 1] := 4;
+	player.block_positions[2, 2] := 1;
+	player.block_positions[3, 1] := 6;
+	player.block_positions[3, 2] := 1;
+	player.block_positions[4, 1] := 4;
+	player.block_positions[4, 2] := 2;
+end;
+
+procedure create_o_block(var player : tetramino);
+begin
+	player.block_positions[1, 1] := 4;
+	player.block_positions[1, 2] := 1;
+	player.block_positions[2, 1] := 5;
+	player.block_positions[2, 2] := 1;
+	player.block_positions[3, 1] := 4;
+	player.block_positions[3, 2] := 2;
+	player.block_positions[4, 1] := 5;
+	player.block_positions[4, 2] := 2;
+end;
+
+procedure create_s_block(var player : tetramino);
+begin
+	player.block_positions[1, 1] := 5;
+	player.block_positions[1, 2] := 1;
+	player.block_positions[2, 1] := 6;
+	player.block_positions[2, 2] := 1;
+	player.block_positions[3, 1] := 5;
+	player.block_positions[3, 2] := 2;
+	player.block_positions[4, 1] := 4;
+	player.block_positions[4, 2] := 2;
+end;
+
+procedure create_z_block(var player : tetramino);
+begin
+	player.block_positions[1, 1] := 5;
+	player.block_positions[1, 2] := 1;
+	player.block_positions[2, 1] := 4;
+	player.block_positions[2, 2] := 1;
+	player.block_positions[3, 1] := 5;
+	player.block_positions[3, 2] := 2;
+	player.block_positions[4, 1] := 6;
+	player.block_positions[4, 2] := 2;
+end;
+
+procedure spawn_tetramino(var player : tetramino; piece_type : integer);
+begin
+	case (piece_type) of
+		0 : create_i_block(player);
+		1 : create_j_block(player);
+		2 : create_l_block(player);
+		3 : create_o_block(player);
+		4 : create_s_block(player);
+		5 : create_t_block(player);
+		6 : create_z_block(player);
 	end;
 end;
 
-procedure move_player(positions: matrix; var x, y: integer; ch : char);
+{	END OF TETRAMINO CREATION	}
+
+procedure insert_block(var positions : matrix; player : tetramino; offset : integer);
+var i : integer;
 begin
-	if (ch = 'a') and (x > 1) and (positions[x - 1, y] = 0) and (positions[x - 1, y +1] = 0) and (positions[x - 1, y + 2] = 0) then
-		x := x - 1
-	else if (ch = 'd') and (x < WIDTH) and (positions[x + 1, y] = 0) and (positions[x + 1, y + 1] = 0) and (positions[x + 1, y + 2] = 0) then
-		x := x + 1;	
+	for i:=1 to 4 do
+		positions[player.block_positions[i, 1], player.block_positions[i, 2] + offset] := 1;
+	fpSystem('spd-say boop');
+end;
+
+procedure check_collision(var positions: matrix; var player : tetramino);
+var x, y, i: integer;
+begin
+	for i:=1 to 4 do
+	begin
+		x := player.block_positions[i, 1];
+		y := player.block_positions[i, 2];
+
+		if positions[x, y + 2] = 1 then
+		begin
+			insert_block(positions, player, 1);
+			spawn_tetramino(player, round(Random) mod 7);
+		end;
+	end;
+end;
+
+procedure player_fall(var player : tetramino; var positions : matrix);
+var i : integer; found : boolean;
+begin
+
+	found := false;
+
+	for i:=1 to 4 do
+	begin
+		if player.block_positions[i, 2] >= HEIGHT - 2 then
+		begin
+			insert_block(positions, player, 2);
+			spawn_tetramino(player, round(Random) mod 7);
+			found := true;
+		end;
+	end;
+
+	if found = false then
+		for i:=1 to 4 do
+			player.block_positions[i, 2] := player.block_positions[i, 2] + 1;
+end;
+
+function check_wall_left(var player : tetramino; var positions) : boolean;
+var i : integer;
+begin
+	check_wall_left := true;
+	for i:=1 to 4 do
+	begin
+		if player.block_positions[i, 1] <= 1 then
+			check_wall_left := false;
+	end;
+end;
+
+function check_wall_right(var player : tetramino; var positions) : boolean;
+var i : integer;
+begin
+	check_wall_right := true;
+	for i:=1 to 4 do
+	begin
+		if player.block_positions[i, 1] >= WIDTH then
+			check_wall_right := false;
+	end;
+end;
+
+procedure move_player(positions: matrix; var blocks: matrix; ch : char; var player : tetramino);
+var i, x, y : integer;
+begin
+
+	if (ch = 'a') and check_wall_left(player, positions) then
+	begin
+		for i:=1 to 4 do
+		begin
+			x := player.block_positions[i, 1];
+			y := player.block_positions[i, 2];
+			if (positions[x - 1, y] = 0) and (positions[x - 1, y +1] = 0) and (positions[x - 1, y + 2] = 0) then
+				player.block_positions[i, 1] := x - 1;
+		end;
+	end
+	else if (ch = 'd') and check_wall_right(player, positions) then
+	begin
+		for i:=1 to 4 do
+		begin
+			x := player.block_positions[i, 1];
+			y := player.block_positions[i, 2];
+			if (positions[x + 1, y] = 0) and (positions[x + 1, y + 1] = 0) and (positions[x + 1, y + 2] = 0) then
+				player.block_positions[i, 1] := x + 1;
+		end;
+	end;
 end;
 
 procedure render_blocks(posits : matrix);
@@ -75,7 +231,17 @@ begin
 		gotoxy(i, HEIGHT); 
 		write('T');
 	end;	
-end;	
+end;
+
+procedure render_player(player : tetramino);
+var i : integer;
+begin
+	for i:=1 to 4 do
+	begin
+		GotoXY(player.block_positions[i,1], player.block_positions[i,2]);
+		write('#');
+	end;
+end;
 
 procedure empty_matrix(var positions : matrix);
 var i, j : integer;
@@ -134,8 +300,10 @@ end;
 { MAIN FUNCTION }
 
 begin
-	x := 3;
-	y := 4;
+	Randomize;
+
+	spawn_tetramino(current_block, round(Random) mod 7);
+
 	timer := 0;
 
 	empty_matrix(positions);
@@ -164,14 +332,14 @@ begin
 		begin
 			canPress := false;
 			ch := ReadKey;
-			check_collision(positions, x, y);
-			move_player(positions, x, y, ch);
+			check_collision(positions, current_block);
+			move_player(positions, current_block.block_positions, ch, current_block);
 		end;	
 		
-		if timer = 10 then
+		if timer = 20 then
 		begin
-			player_fall(x, y, positions);
-			check_collision(positions, x, y);
+			player_fall(current_block, positions);
+			check_collision(positions, current_block);
 			timer := 0;
 		end
 		else
@@ -180,8 +348,7 @@ begin
 		clrscr;
 		erase_line(positions);
 		render_blocks(positions);
-		GotoXY(x, y);
-		write('#');
+		render_player(current_block);
 		GotoXY(1, 12);
 		write('By Isoo');
 	end;
