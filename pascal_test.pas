207682e1e8aa3196994d61ea6410e3c725f707ec
Gustavo Isoo Binder <gustan.binder@gmail.com>
2023-05-24 14:57:36 -0300

    Feat: better rendering + speed increase

--- pascal_test.pas
+++ pascal_test.pas
@@ -6,12 +6,12 @@ const WIDTH = 10; HEIGHT = 21;
 type matrix = array[1..10, 1..21] of integer;
 
 type tetramino = record
-		piece_type, middle_x, middle_y : integer;
+		color : integer;
 		block_positions : matrix; {	FIRST ROW IS ALWAYS THE MIDDLE BLOCK }
 		end;
 
 var ch: char;
-timer, score: integer;
+timer, timerSet, score: integer;
 canPress, paused : boolean;
 positions : matrix;
 current_block : tetramino;
@@ -28,6 +28,7 @@ begin
 	player.block_positions[4, 1] := 7;
 	for i:=1 to 4 do
 		player.block_positions[i, 2] := 1;
+	player.color := GREEN;
 end;
 
 procedure create_t_block(var player : tetramino);
@@ -40,6 +41,7 @@ begin
 	player.block_positions[3, 2] := 1;
 	player.block_positions[4, 1] := 5;
 	player.block_positions[4, 2] := 2;
+	player.color := RED;
 end;
 
 procedure create_l_block(var player : tetramino);
@@ -52,6 +54,7 @@ begin
 	player.block_positions[3, 2] := 1;
 	player.block_positions[4, 1] := 6;
 	player.block_positions[4, 2] := 2;
+	player.color := BLUE;
 end;
 
 procedure create_j_block(var player : tetramino);
@@ -64,6 +67,7 @@ begin
 	player.block_positions[3, 2] := 1;
 	player.block_positions[4, 1] := 4;
 	player.block_positions[4, 2] := 2;
+	player.color := MAGENTA;
 end;
 
 procedure create_o_block(var player : tetramino);
@@ -76,6 +80,7 @@ begin
 	player.block_positions[3, 2] := 2;
 	player.block_positions[4, 1] := 5;
 	player.block_positions[4, 2] := 2;
+	player.color := WHITE;
 end;
 
 procedure create_s_block(var player : tetramino);
@@ -88,6 +93,7 @@ begin
 	player.block_positions[3, 2] := 2;
 	player.block_positions[4, 1] := 4;
 	player.block_positions[4, 2] := 2;
+	player.color := YELLOW;
 end;
 
 procedure create_z_block(var player : tetramino);
@@ -100,6 +106,7 @@ begin
 	player.block_positions[3, 2] := 2;
 	player.block_positions[4, 1] := 6;
 	player.block_positions[4, 2] := 2;
+	player.color := CYAN;
 end;
 
 {	Chooses the block to be created	}
@@ -205,7 +212,7 @@ begin
 		x := player.block_positions[i, 1];
 		y := player.block_positions[i, 2];
 
-		if (x < 1) or (x > WIDTH) or (positions[x, y] = 1) then
+		if (x < 1) or (x > WIDTH) or (positions[x, y] = 1) or (y < 1) or (y > HEIGHT) then
 			occupied := true;
 	end;
 end;
@@ -257,38 +264,41 @@ begin
 		rotate(player, -1, positions);
 end;
 
-procedure render_blocks(posits : matrix);
+procedure render_player(player : tetramino; x, y : integer);
+var i : integer;
+begin
+	for i:=1 to 4 do
+	begin
+		if (player.block_positions[i, 1] = x) and (player.block_positions[i, 2] = y) then
+		begin
+			TextColor(player.color);
+			GotoXY(player.block_positions[i,1] * 2, player.block_positions[i,2]);
+			write('[]');
+			break;
+		end
+		else
+		begin
+			GotoXY(x * 2, y);
+			write('. ');
+		end;
+	end;
+end;
+
+procedure render_blocks(posits : matrix; player : tetramino);
 var i, j : integer;
 begin
-	TextColor(BLACK);
 	for i:=1 to HEIGHT do
 	begin
 	for j:=1 to WIDTH do
 	begin
 		gotoxy(j * 2, i);
+		TextColor(WHITE);
 
 		if posits[j, i] = 1 then
 			write('[]')
 		else
-			write('. ');
+			render_player(player, j, i);
 	end;
-	end;
-
-	{for i:=1 to WIDTH do
-	begin
-		gotoxy(i, HEIGHT); 
-		write('T');
-	end;}	
-end;
-
-procedure render_player(player : tetramino);
-var i : integer;
-begin
-	TextColor(Red);
-	for i:=1 to 4 do
-	begin
-		GotoXY(player.block_positions[i,1] * 2, player.block_positions[i,2]);
-		write('[]');
 	end;
 end;
 
@@ -300,7 +310,7 @@ begin
 			positions[j, i] := 0;
 end;
 
-function line_full(positions : matrix; var line : integer) : boolean;
+function line_full(positions : matrix; var line : integer; player : tetramino) : boolean;
 var i, j, line_to_erase : integer; found : boolean;
 begin
 
@@ -328,17 +338,17 @@ begin
 		for i:=1 to WIDTH do
 			positions[i, line_to_erase] := 0;
 
-		render_blocks(positions);
-		{		fpSystem('spd-say TETRIS');}
-		{fpSystem('notify-send "Line Complete"');}
+		render_blocks(positions, player);
+		delay(150);
 	end;
 end;
 
-procedure erase_line(var positions: matrix);
+procedure erase_line(var positions: matrix; player : tetramino; var timerSet : integer);
 var i, j, line_to_erase : integer;
 begin
-	if line_full(positions, line_to_erase) then
+	if line_full(positions, line_to_erase, player) then
 	begin
+		timerSet := timerSet - 5;
 		j:=line_to_erase;
 		while j > 1 do
 		begin
@@ -361,6 +371,7 @@ begin
 	spawn_tetramino(current_block, round(Random) mod 7);
 
 	timer := 0;
+	timerSet := 175;
 	paused := false;
 
 	empty_matrix(positions);
@@ -371,8 +382,7 @@ begin
 	fpSystem('tput civis');
 	cursoroff;
 	Window(1, 1, WIDTH * 2 + 30, HEIGHT + 1);
-	TextBackground(WHITE);
-	TextColor(BLUE);
+	TextBackground(BLACK);
 
 	writeln('ТЕТРИС');
 
@@ -402,7 +412,7 @@ begin
 				move_player(positions, current_block.block_positions, ch, current_block);
 		end;
 		
-		if (timer = 200) and (not paused)  then
+		if (timer = timerSet) and (not paused)  then
 		begin
 			player_fall(current_block, positions);
 			check_collision(positions, current_block);
@@ -411,22 +421,22 @@ begin
 		else if (not paused) then
 			timer := timer + 1;
 			
-		erase_line(positions);
-		render_blocks(positions);
-		render_player(current_block);
+		erase_line(positions, current_block, timerSet);
+		render_blocks(positions, current_block);
+		TextColor(RED);
 		GotoXY(23, 2);
 		write('Score: ', score);
 		GotoXY(23, 6);
 		if paused then
 		begin
-			GotoXY(32, 10);
+			GotoXY(8, 10);
 			write('PAUSED');
 		end
 		else
 		begin
-			write('WASD to Move, P to Pause');
-			GotoXY(32, 10);
-			write('       ')
+			write('AD to Move and SW to Rotate');
+			GotoXY(23, 7);
+			write('Press P to Pause');
 		end;
 	end;
 
